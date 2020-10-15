060b55c9199db774907b55ad6306766bbd7b4aee
user <user@DESKTOP-EHCAT36>
2020-10-15 19:43:40 +0530

    commit

--- Instagram/src/insta/Follow.java
+++ Instagram/src/insta/Follow.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,17 +17,20 @@ public class Follow {
 		//credentials
 		String username="kuntal7875";
 		String password="kuntal123";
+
+		WebDriver driver=new ChromeDriver();	//open browser
+		driver.manage().window().maximize();	//maximize window
 		
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.instagram.com/accounts/login/?hl=en");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.instagram.com/accounts/login/?hl=en");	//website
 		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);	//wait
+
 		//login
 		driver.findElement(By.xpath("//input[@aria-label='Phone number, username, or email']")).sendKeys(username);
 		driver.findElement(By.xpath("//input[@aria-label='Password']")).sendKeys(password);
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 
+		//handle save password & notification
 		Thread.sleep(3000);
 		String title = driver.getTitle();
 		for(int i=0;i<2;i++) {
@@ -37,6 +41,19 @@ public class Follow {
 		}
 		driver.findElement(By.xpath("//div[.='See All']")).click();
 		
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		
+		//scroll down
+		Thread.sleep(500);
+		for(int i=0;i<4;i++) {
+			js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+			Thread.sleep(500);
+		}
+
+		//scroll up
+		js.executeScript("window.scrollTo(0, 0)");
+		Thread.sleep(500);
+		
 		//follow
 		Thread.sleep(3000);
 		List<WebElement> follow = driver.findElements(By.xpath("//div[@class='                    Igw0E   rBNOH        eGOV_     ybXk5    _4EzTm                                                                                   XfCBB          HVWg4                 ']/div[3]/button[.='Follow']"));
@@ -45,10 +62,10 @@ public class Follow {
 		for(WebElement f:follow) {
 			f.click();
 		}
-		
+
 		//logout
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//div[@class='MWDvN buoMu nfCOa']/div[3]//img[@data-testid='user-avatar']")).click();
+		driver.findElement(By.xpath("//div[@class='MWDvN buoMu nfCOa']/div[1]/a")).click();	//go to homepage
+		driver.findElement(By.xpath("//div[@class='MWDvN  nfCOa']/div[3]/div/div[5]//img[@data-testid='user-avatar']")).click();
 		driver.findElement(By.xpath("//div[@class='-qQT3']/div[.='Log Out']")).click();
 		driver.close();
 	}
--- Instagram/src/insta/FollowFromProfile.java
+++ Instagram/src/insta/FollowFromProfile.java
@@ -16,13 +16,14 @@ public class FollowFromProfile {
 		//credentials
 		String username="kuntal7875";
 		String password="kuntal123";
-		String NameOfProfile="arianagrande";
-		
+
+		String NameOfProfile="technicalguruji";
+
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.instagram.com/accounts/login/?hl=en");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
+
 		//login
 		driver.findElement(By.xpath("//input[@aria-label='Phone number, username, or email']")).sendKeys(username);
 		driver.findElement(By.xpath("//input[@aria-label='Password']")).sendKeys(password);
@@ -39,14 +40,49 @@ public class FollowFromProfile {
 
 		//search bar
 		driver.findElement(By.xpath("//div[@class='MWDvN  nfCOa']/div[2]/input")).sendKeys(NameOfProfile);
-		
+
 		//click on profile
 		driver.findElement(By.xpath("(//div[@class='fuqBx']/a)[1]")).click();
-		
+
+		//first post
+		driver.findElement(By.xpath("(//div[@class=' _2z6nI']/article/div/div/div/div/a/div/div[2])[1]")).click();
+
+
+		try {
+			driver.findElement(By.xpath("(//button[@class='sqdOP yWX7d     _8A5w5    '])[2]")).click();//others btn	
+		}
+		catch(Exception e){
+			driver.findElement(By.xpath("//div[@class='Nm9Fw']/button")).click();//like btn
+		}
+
+		//loop for following many people
+		for(int i=0;i<4;i++) {
+
+			//follow
+			Thread.sleep(3000);
+			List<WebElement> follow = driver.findElements(By.xpath("//div[@class='                    Igw0E   rBNOH        eGOV_     ybXk5    _4EzTm                                                                                   XfCBB          HVWg4                 ']/div[3]/button[.='Follow']"));
+			int x = follow.size();
+			System.out.println("You followed "+x+" people.");
+			for(WebElement f:follow) {
+				f.click();
+			}
+
+			//"We restrict certain activity to protect our community" popup
+			Thread.sleep(500);
+			try {
+				driver.findElement(By.xpath("//button[.='OK']")).click();
+			}
+			catch(Exception e) {}
+			driver.findElement(By.xpath("(//button[@class='wpO6b ']/div[@class='QBdPU '])[18]")).click();//close popup:follow
+		}
+
+		driver.findElement(By.xpath("(//button[@class='wpO6b ']/div[@class='QBdPU '])[19]")).click();//close popup:post
+
 		//logout
-//		Thread.sleep(3000);
-//		driver.findElement(By.xpath("//div[@class='MWDvN buoMu nfCOa']/div[3]//img[@data-testid='user-avatar']")).click();
-//		driver.findElement(By.xpath("//div[@class='-qQT3']/div[.='Log Out']")).click();
-//		driver.close();
+		driver.findElement(By.xpath("//div[@class='MWDvN buoMu nfCOa']/div[1]/a")).click();	//go to homepage
+		driver.findElement(By.xpath("//div[@class='MWDvN  nfCOa']/div[3]/div/div[5]//img[@data-testid='user-avatar']")).click();
+		driver.findElement(By.xpath("//div[@class='-qQT3']/div[.='Log Out']")).click();
+		driver.close();
 	}
 }
+
